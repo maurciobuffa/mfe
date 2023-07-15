3a89059c950bbdbfa909045c1645cdde3cab1efd
Mauricio Buffa <mauriciobuffa@MacBook-Pro-de-Mauricio.local>
2023-07-15 12:09:10 -0300

    fix impor

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,9 +2,9 @@ import React from 'react'
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
 import { BrowserRouter } from 'react-router-dom'
-import {StylesProvider, createGeneratedClassName} from '@material-ui/core/styles';
+import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles';
 
-const generateClassName = createGeneratedClassName({
+const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
 })
 
